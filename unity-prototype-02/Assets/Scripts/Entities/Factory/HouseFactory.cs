1d653b9743318106c4194e8870c1c63aa7d27d35
Jeff <jeff@renderheads.com>
2023-08-22 22:27:35 +0400

    adds human/house

---
+++ unity-prototype-02/Assets/Scripts/Entities/Factory/HouseFactory.cs
@@ -0,0 +1,30 @@
+//RenderHeads - Jeff Rusch
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RenderHeads
+{
+    public class HouseFactory : FactoryEntity
+    {
+        #region Public Properties
+
+        #endregion
+
+        #region Private Properties
+
+        #endregion
+
+        #region Public Methods
+        public void Start()
+        {
+            Init();
+            Factory.AddBehaviour(new FactoryBehaviourSpawnHuman());
+        }
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
